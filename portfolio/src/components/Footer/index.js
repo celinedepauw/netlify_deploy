da98343b22efb1e89d8ec106a1b959d5e480406a
celin <celin@localhost>
2021-06-11 17:26:24 +0200

    component Footer ok except linkedin icon

--- portfolio/src/components/Footer/index.js
+++ portfolio/src/components/Footer/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
 import './footer.scss';
 
 const Footer = () => (
         <footer className="footer">
+            <div className="footer_contact">Contact</div>
             <div className="footer_email">celinedepauw2@gmail.com</div>
-            <div className="footer_linkedin">Icone lien LinkedIn</div>
+            <a href="https://www.linkedin.com/in/celinedepauw/" className="footer_linkedin">Linkedin</a>
         </footer>
 );
 
