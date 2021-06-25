f83480c52234bcde9e0ad54b2857d3cc57c077ee
celin <celin@localhost>
2021-06-25 11:00:25 +0200

    link to resume

--- portfolio/src/components/Home/ResumeSmall.js
+++ portfolio/src/components/Home/ResumeSmall.js
@@ -5,7 +5,7 @@ const ResumeSmall = () => (
     <div className="resumeSmall">
         <h3 className="resumeSmall_title">Mon CV</h3>
         <p className="resumeSmall_intro">A peu près tout ce que vous venez de voir est récapitulé dans mon CV, sur une seule page biensûr !</p>
-        <p className="resumeSmall_link"><Link to="/cv">Voir le CV</Link></p>
+        <a href="https://www.canva.com/design/DAEXtkdLzpI/V9oH1MMyO2PCxNRszpi3tA/view?utm_content=DAEXtkdLzpI&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink" target="_blank" className="resumeSmall_link">Voir le CV</a>
     </div>
 );
 
