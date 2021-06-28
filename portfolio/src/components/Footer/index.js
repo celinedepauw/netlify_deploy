3cfd300408e6a9bdeb52eecf0f56049adcd318b9
celin <celin@localhost>
2021-06-28 14:32:51 +0200

    add link to github in footer

--- portfolio/src/components/Footer/index.js
+++ portfolio/src/components/Footer/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import email from '../../assets/images/email.svg';
 import linkedin from '../../assets/images/linkedin.png';
+import github from '../../assets/images/github.png';
+
 import './footer.scss';
 
 const Footer = () => (
         <footer className="footer">
-            <div className="footer_contact">Contact</div>
             <div className="footer_email">
                 <img src={email} className="footer_email_picture" />
                 <div className="footer_email_text">celinedepauw2@gmail.com</div>
             </div>
             <a href="https://www.linkedin.com/in/celinedepauw/" target="_blank" className="footer_linkedin_link"><img src={linkedin} className="footer_linkedin_logo" /></a>
+            <a href="https://github.com/celinedepauw" target="_blank" className="footer_github_link"><img src={github} className="footer_github_logo" /></a>
         </footer>
 );
 
