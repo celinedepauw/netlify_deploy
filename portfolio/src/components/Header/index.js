b93cd671523c993b698a77b34590e31681defb02
celin <celin@localhost>
2021-06-28 17:03:34 +0200

    logo rainbow in header

--- portfolio/src/components/Header/index.js
+++ portfolio/src/components/Header/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import profile from '../../assets/images/celine.png';
+import profile from '../../assets/images/profile.png';
+import rainbow from '../../assets/images/rainbow.png';
 
 import './header.scss';
 
@@ -8,7 +9,7 @@ const Header = () => (
         <header className="header">
             <Link to="/" className="header_return_link">
                 <img src={profile} alt="profil" className="header_picture" />
-                <h1 className="header_name">Céline Depauw</h1>
+                <h1 className="header_name">Céline Depauw <img src={rainbow} className="header_logo" /></h1>
                 <div className="header_job">Développeuse Web et Web Mobile</div>
             </Link>
         </header>
