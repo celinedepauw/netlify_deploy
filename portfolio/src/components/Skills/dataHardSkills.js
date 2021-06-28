e76c253ca18c6c7aeddb939af9fb7c83954365cd
celin <celin@localhost>
2021-06-28 13:59:19 +0200

    logos for hardskills ok

--- portfolio/src/components/Skills/dataHardSkills.js
+++ portfolio/src/components/Skills/dataHardSkills.js
@@ -1,34 +1,38 @@
 export const datasHardSkills = [
     {
-        name: "HTML5 / CSS3",
-        logo: "",
+        name: "HTML5",
+        logo: "https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748__340.png",
     },
     {
-        name: "JS",
+        name: "CSS3",
+        logo: "https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_960_720.png",
+    },
+    {
+        name: "Javascript",
         logo: "https://cdn.pixabay.com/photo/2015/04/23/17/41/javascript-736400_960_720.png",
     },
     {
         name: "React",
-        logo: "https://images.assetsdelivery.com/compings_v2/shoutinglime/shoutinglime2103/shoutinglime210300005.jpg",
+        logo: "http://assets.stickpng.com/thumbs/584830f5cef1014c0b5e4aa1.png",
     },
     {
         name: "Redux",
-        logo: "",
+        logo: "http://assets.stickpng.com/thumbs/5848309bcef1014c0b5e4a9a.png",
     },
     {
         name: "Sass",
-        logo: "",
+        logo: "https://image.flaticon.com/icons/png/512/919/919831.png",
     },
     {
         name: "Bootstrap",
-        logo: "",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/512px-Bootstrap_logo.svg.png",
     },
     {
         name: "PHP",
-        logo: "",
+        logo: "http://assets.stickpng.com/thumbs/58481791cef1014c0b5e4994.png",
     },
     {
         name: "MySql",
-        logo: "",
+        logo: "http://assets.stickpng.com/thumbs/58481057cef1014c0b5e4951.png",
     },
 ];
