258a60b96b2274b92991715f087aebdd7fb5e211
celin <celin@localhost>
2021-06-28 18:30:41 +0200

    css in header

--- portfolio/src/components/Header/index.js
+++ portfolio/src/components/Header/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import profile from '../../assets/images/profile.png';
-import rainbow from '../../assets/images/rainbow.png';
 
 import './header.scss';
 
