073330a6bf175990f1fbb0fa7543be44c39f0f28
celin <celin@localhost>
2021-06-28 18:26:39 +0200

    header smaller

--- portfolio/src/components/Header/index.js
+++ portfolio/src/components/Header/index.js
@@ -8,8 +8,7 @@ import './header.scss';
 const Header = () => (
         <header className="header">
             <Link to="/" className="header_return_link">
-                <img src={profile} alt="profil" className="header_picture" />
-                <h1 className="header_name">Céline Depauw <img src={rainbow} className="header_logo" /></h1>
+                <h1 className="header_name">Céline Depauw <img src={profile} className="header_picture" /></h1>
                 <div className="header_job">Développeuse Web et Web Mobile</div>
             </Link>
         </header>
