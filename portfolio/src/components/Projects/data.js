f81765f042a1f2e7e2388db93cc90eb27f41f999
celin <celin@localhost>
2021-06-29 14:58:43 +0200

    add icons for projects

--- portfolio/src/components/Projects/data.js
+++ portfolio/src/components/Projects/data.js
@@ -3,14 +3,14 @@ export const datas = [
         title: "Orando",
         subtitle: "React-Redux & PHP",
         description: "Site web responsive dédiée à la randonnée, développé de A à Z au sein d'une équipe de 5 développeurs. Je faisais partie de l'équipe front-end. Les principales fonctionnalités sont : consultation d'une randonnée, création et modification de profil, création et modification d'une randonnée, participation à une randonnée, contact d'un organisateur, consultation des participants à une randonnée...",
-        image: "thhhhh",
+        image: "https://image.flaticon.com/icons/png/512/2826/2826746.png",
         link: "",
     },
     {
         title: "Movies",
         subtitle: "PHP & Bootstrap",
         description: "Site web développé pour pratiquer les bases de données et pour mettre en application ce que j'avais appris en début de formation. Il recense tous les dvd qu'on a à la maison, classés par catégories.",
-        image: "ert",
+        image: "https://image.flaticon.com/icons/png/512/3791/3791709.png",
         link: "",
     },
 ];
--- portfolio/src/components/Projects/index.js
+++ portfolio/src/components/Projects/index.js
@@ -13,7 +13,10 @@ const Projects = () => (
                     className="project"
                     key={project.title}
                 >
-                    <h3 className="project_title">{project.title}</h3>
+                    <div className="project_title">
+                        <h3 className="project_title_text">{project.title}</h3>
+                        <img src={project.image} alt="projet_illustration" className="project_title_icon"/>
+                    </div>
                     <p className="project_subtitle">{project.subtitle}</p>
                     <p className="project_description">{project.description}</p>
                     <p className="project_link"><Link to={`/projet_${project.title}`}>Voir le projet</Link></p>
