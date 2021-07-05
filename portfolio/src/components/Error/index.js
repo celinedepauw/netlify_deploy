a3be698a82c73a455b79abd6f5a3488a40842b4d
celin <celin@localhost>
2021-07-05 17:56:16 +0200

    css in error page

--- portfolio/src/components/Error/index.js
+++ portfolio/src/components/Error/index.js
@@ -6,7 +6,7 @@ const Error = () => (
   <div className="error">
     <Link to="/" className="error_return">Retour page accueil</Link>
     <h2 className="error_title">Page introuvable</h2>
-    <p className="error_text">Oups ! il y a une erreur de chemin...</p>
+    <p className="error_text">Oups ! La page demandée n'existe pas, il doit y avoir une erreur de chemin...</p>
   </div>
 );
 
