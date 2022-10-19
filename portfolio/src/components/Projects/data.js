f76f86f4f0efcaeb41dcbba2a9988c3402c282cd
celin <celin@localhost>
2022-10-19 15:24:20 +0200

    change in orando description

--- portfolio/src/components/Projects/data.js
+++ portfolio/src/components/Projects/data.js
@@ -23,7 +23,7 @@ export const datas = [
     {
         title: "Orando",
         subtitle: "React-Redux & PHP",
-        description: "Site web responsive dédiée à la randonnée, développé de A à Z au sein d'une équipe de 5 développeurs. Je faisais partie de l'équipe front-end. Les principales fonctionnalités sont : consultation d'une randonnée, création et modification de profil, création et modification d'une randonnée, participation à une randonnée, contact d'un organisateur, consultation des participants à une randonnée...",
+        description: "Site web responsive dédiée à la randonnée, développé de A à Z au sein d'une équipe de 5 développeurs. Je faisais partie de l'équipe front-end. Les principales fonctionnalités sont : consultation d'une randonnée, création et modification de profil, création et modification d'une randonnée, participation à une randonnée, contact d'un organisateur, consultation des participants à une randonnée, etc. J'occupais également le poste de Product Owner : définition du besoin, écriture et priorisation des US, suivi de l'avancement du projet.",
         image: "https://cdn-icons-png.flaticon.com/128/6167/6167576.png",
         date: "Mars - Avril 2021",
     },
